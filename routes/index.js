3ec33f62fb8e429dd93b70fb07878674227a40cf
Brian Hamilton <hmltnbrn@gmail.com>
2017-04-26 16:50:24 -0400

    Forgot password page

--- routes/index.js
+++ routes/index.js
@@ -161,6 +161,12 @@ router.get('/signup', isNotLoggedIn, function (req, res, next) {
   });
 });
 
+router.get('/forgotpassword', isNotLoggedIn, queries.getPoem, function (req, res, next) {
+  return res.render('pages/forgotpassword', {
+    username: req.user ? req.user.username : null
+  });
+});
+
 router.get('/login', isNotLoggedIn, function (req, res, next) {
   return res.render('pages/login', {
     message: req.flash('loginMessage'),
